dc4727182b1b5138c506d782976f591373a9f10d
Geoff <George.Jukes@googlemail.com>
2022-05-03 18:24:04 +0100

    Remove files from directory and remove package information to fix plugin showing 4 times

--- nla-im-button-embed/nla-im-button-embed-shortcodes.php
+++ nla-im-button-embed/nla-im-button-embed-shortcodes.php
@@ -1,14 +1,4 @@
 <?php
-/*
-Plugin Name:  NLA IM Button Embed Plugin
-Plugin URI:   https://nightline.ac.uk
-Description:  Enables easy embedding of IM buttons onto wordpress sites.
-Version:      0.1
-Author:       Geoff Jukes
-Author URI:   https://nightline.ac.uk
-License:      GPL2
-License URI:  https://www.gnu.org/licenses/gpl-2.0.html
-*/
 
 require_once( NLA_TOOLS__PLUGIN_DIR . 'nla-im-button-embed-functions.php' );
 
