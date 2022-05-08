ec5b85fdb713a9ae923f4317b8c3b9094f054c4d
Oliver Earl <oliver@oliverearl.co.uk>
2022-05-08 17:38:30 +0100

    Replace version.

--- nla-im-button-embed/nla-im-button-embed.php
+++ nla-im-button-embed/nla-im-button-embed.php
@@ -3,7 +3,7 @@
 Plugin Name:  NLA IM Button Embed Plugin
 Plugin URI:   https://nightline.ac.uk
 Description:  Enables easy embedding of IM buttons onto WordPress sites.
-Version:      0.3
+Version:      0.2.1
 Author:       Nightline Association
 Author URI:   https://nightline.ac.uk
 License:      GPL2
@@ -11,6 +11,7 @@ License URI:  https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 define( 'NLA_TOOLS__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'NLA_TOOLS__PLUGIN_VER', '0.2.1' );
 
 require_once NLA_TOOLS__PLUGIN_DIR . 'nla-im-button-embed-functions.php';
 require_once NLA_TOOLS__PLUGIN_DIR . 'nla-im-button-embed-shortcodes.php';
